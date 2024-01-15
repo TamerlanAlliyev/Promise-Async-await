e609c404aeaee2fe9639725dfa9b052749d8d29e
Tamerlan Aliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2024-01-15 10:47:22 +0400

    Update script.js

--- DevFinder/script.js
+++ DevFinder/script.js
@@ -20,3 +20,51 @@ const loading = document.getElementById("loading");
 const errorEl = document.getElementById("error");
 
 let dev;
+
+const fetchUser = (developer) => {
+    loading.classList.remove('hidden');
+    fetch(`https://api.github.com/users/${developer}`)
+        .then((response) => {
+            if (!response.ok) {
+                errorEl.classList.remove('hidden');
+                result.classList.add('hidden');
+                throw new Error("Couldn't find any user");
+            } else {
+                errorEl.classList.add('hidden');
+                result.classList.remove('hidden');
+                return response.json();
+            }
+        })
+        .then((data) => {
+            loading.classList.add('hidden');
+            displayUser(data);
+        })
+        .catch((error) => {
+            console.error(error); 
+            loading.classList.add('hidden');
+        });
+};
+
+btn.addEventListener('click', () => {
+    result.classList.add('hidden');
+    const givenValue = input.value.trim();
+    if (givenValue) {
+        fetchUser(givenValue);
+    }
+    input.value = "";
+});
+
+const displayUser = (userData) => {
+    const indexOfT = userData.created_at.indexOf("T");
+    repos.textContent = userData.public_repos;
+    followers.textContent = userData.followers;
+    followings.textContent = userData.following;
+    bio.textContent = !userData.bio ? "No Bio" : userData.bio;
+    username.textContent = "@" + userData.login;
+    username.href = userData.html_url;
+    img.src = userData.avatar_url;
+    user.textContent = userData.name;
+    joined.textContent = `Joined ${userData.created_at.slice(0, indexOfT)}`;
+};
+
+console.log("a");
